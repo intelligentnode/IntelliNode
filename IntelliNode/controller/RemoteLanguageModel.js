c624669f48877b3234bfb7260651c198a4b291b3
Ahmad Barqawi <Barqawi.88@outlook.com>
2023-04-23 21:11:58 +0100

    remote language models implementation

---
+++ IntelliNode/controller/RemoteLanguageModel.js
@@ -0,0 +1,75 @@
+const OpenAIWrapper = require('../wrappers/OpenAIWrapper');
+const CohereAIWrapper = require('../wrappers/CohereAIWrapper');
+const LanguageModelInput = require('../model/input/LanguageModelInput');
+
+const SupportedLangModels = {
+  OPENAI: 'openai',
+  COHERE: 'cohere',
+};
+
+class RemoteLanguageModel {
+  constructor(keyValue, provider) {
+    if (!provider) {
+      provider = SupportedLangModels.OPENAI;
+    }
+
+    const supportedModels = this.getSupportedModels();
+
+    if (supportedModels.includes(provider)) {
+      this.initiate(keyValue, provider);
+    } else {
+      const models = supportedModels.join(' - ');
+      throw new Error(`The received keyValue is not supported. Send any model from: ${models}`);
+    }
+  }
+
+  initiate(keyValue, keyType) {
+    this.keyType = keyType;
+
+    if (keyType === SupportedLangModels.OPENAI) {
+      this.openaiWrapper = new OpenAIWrapper(keyValue);
+    } else if (keyType === SupportedLangModels.COHERE) {
+      this.cohereWrapper = new CohereAIWrapper(keyValue);
+    } else {
+      throw new Error('Invalid provider name');
+    }
+  }
+
+  getSupportedModels() {
+    return Object.values(SupportedLangModels);
+  }
+
+  async generateText(langInput) {
+
+    let inputs;
+
+    if (langInput instanceof LanguageModelInput) {
+      if (this.keyType === SupportedLangModels.OPENAI) {
+        inputs = langInput.getOpenAIInputs();
+      } else if (this.keyType === SupportedLangModels.COHERE) {
+        inputs = langInput.getCohereInputs();
+      } else {
+        throw new Error('The keyType is not supported');
+      }
+    } else if (typeof langInput === 'object') {
+      inputs = langInput;
+    } else {
+      throw new Error('Invalid input: Must be an instance of LanguageModelInput or a dictionary');
+    }
+
+    if (this.keyType === SupportedLangModels.OPENAI) {
+      const results = await this.openaiWrapper.generateText(inputs);
+      return results.choices.map((choice) => choice.text);
+    } else if (this.keyType === SupportedLangModels.COHERE) {
+      const results = await this.cohereWrapper.generateText(inputs);
+      return results.generations.map((generation) => generation.text);
+    } else {
+      throw new Error('The keyType is not supported');
+    }
+  }
+}
+
+module.exports = {
+  RemoteLanguageModel,
+  SupportedLangModels,
+};
---
+++ IntelliNode/test/RemoteLanguageModel.test.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const assert = require('assert');
+const { RemoteLanguageModel, SupportedLangModels } = require('../controller/RemoteLanguageModel');
+const LanguageModelInput = require('../model/input/LanguageModelInput');
+
+const openaiApiKey = process.env.OPENAI_API_KEY;
+const cohereApiKey = process.env.COHERE_API_KEY;
+
+const openaiLanguageModel = new RemoteLanguageModel(openaiApiKey, SupportedLangModels.OPENAI);
+const cohereLanguageModel = new RemoteLanguageModel(cohereApiKey, SupportedLangModels.COHERE);
+
+async function testOpenAIGenerateOneOutput() {
+  const langInput = new LanguageModelInput({
+    prompt: 'Write a product description for any device input adapter.',
+    model: 'text-davinci-003',
+    temperature: 0.7});
+
+  //console.log('openAI inputs: ', langInput.getOpenAIInputs());
+
+  const results = await openaiLanguageModel.generateText(langInput);
+  console.log('OpenAI Generate One Output:', results[0]);
+  assert(results.length > 0, 'Test passed');
+}
+
+async function testOpenAIGenerateMultipleOutputs() {
+  const langInput = new LanguageModelInput({
+          prompt:'Write a product description for any device input adapter.',
+          model:'text-davinci-003',
+          numberOfOutputs:3,
+          temperature:0.7})
+
+  //console.log('openAI inputs: ', langInput.getOpenAIInputs());
+
+  
+  const results = await openaiLanguageModel.generateText(langInput);
+  console.log('\nOpenAI Generate Multiple Outputs:', results);
+  assert(results.length > 0, 'Test passed');
+}
+
+async function testCohereGenerateOneOutput() {
+  const langInput = new LanguageModelInput({prompt:'Write a product description for any device input adapter.'});
+  langInput.setDefaultValues(SupportedLangModels.COHERE);
+
+  // console.log('cohere inputs: ', langInput.getCohereInputs());
+
+  const results = await cohereLanguageModel.generateText(langInput);
+  console.log('\nCohere Generate One Output:', results[0]);
+  assert(results.length > 0, 'Test passed');
+}
+
+(async () => {
+  await testOpenAIGenerateOneOutput();
+  await testOpenAIGenerateMultipleOutputs();
+  await testCohereGenerateOneOutput();
+})();
