330d54fb986f1038399a5b6cda9aaae925b321be
Ahmad Barqawi <Barqawi.88@outlook.com>
2023-04-24 16:45:11 +0100

    update the speech synthesize example
    save the output as mp3 file.

--- samples/command_sample/test_speech_models.js
+++ samples/command_sample/test_speech_models.js
@@ -1,15 +1,23 @@
-const { RemoteSpeechModel, Text2SpeechInput } = require('intellinode');
+const { RemoteSpeechModel, Text2SpeechInput, AudioHelper } = require('intellinode');
+
+const audioHelper = new AudioHelper();
 
 async function generateSpeech(apiKey, text, language) {
   const speechModel = new RemoteSpeechModel(apiKey);
   const input = new Text2SpeechInput({ text: text, language: language });
   const audioContent = await speechModel.generateSpeech(input);
-  console.log('Generated speech:', audioContent);
+
+  const decodedAudio = audioHelper.decode(audioContent);
+
+  const saved = audioHelper.saveAudio(decodedAudio, './temp', 'temp.mp3');
+  console.log(`Audio file saved: ${saved}`);
+  console.log('check the temp folder')
+
 }
 
 (async () => {
   // Generate speech
-  const apiKey = 'your-api-key';
+  const apiKey = 'AIzaSyDDfcAX_9oinw015LKZxmP9s1bTgM1q3yE';
   const text = 'Welcome to Intelligent Node';
   const language = 'en-gb';
 
