ed02b9722339a222a9b815612ec4aad3c04565e6
Ahmad Barqawi <Barqawi.88@outlook.com>
2023-04-25 23:40:24 +0100

    add shiba inu image generation example

---
+++ samples/command_sample/shiba_image_generator.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const { RemoteImageModel, SupportedImageModels, ImageModelInput } = require('intellinode');
+const dotenv = require('dotenv');
+dotenv.config();
+
+async function generateImages(apiKey, provider, imageInput) {
+  const imgModel = new RemoteImageModel(apiKey, provider);
+  const images = await imgModel.generateImages(imageInput);
+  console.log(`Generated images (${provider}):`);
+  images.forEach((image, index) => {
+    fs.writeFileSync(`./temp/doge_image${index + 1}.png`, image, { encoding: 'base64' });
+  });
+  console.log('the images saved in the temp folder')
+}
+
+(async () => {
+  prompt = 'A cartoon-style Shiba Inu dog with a playful expression, standing on a patterned background with various dog toys scattered around. The background is filled with colorful paw prints and bones.'
+  // Generate image using Stability
+  const myKey = process.env.OPENAI_API_KEY;
+  const imageInput = new ImageModelInput({
+    prompt: prompt,
+    numberOfImages: 3,
+    size: '512x512',
+    responseFormat:'b64_json'
+  });
+
+  await generateImages(myKey, SupportedImageModels.OPENAI, imageInput);
+})();
