8e5635a7bde4a3692532b3a8046858b8d656c016
Ahmad Barqawi <Barqawi.88@outlook.com>
2023-05-04 22:39:08 +0100

    format the web samples

--- samples/content_generator_ui/app.js
+++ samples/content_generator_ui/app.js
@@ -1,23 +1,25 @@
-const { Gen } = require("intellinode");
+const {
+    Gen
+} = require("intellinode");
 const express = require('express');
 
 const intelliCode = {
-  async generateText(prompt) {
-    const apiKey = "<openai-key>";
-    return await Gen.get_marketing_desc(prompt, apiKey);
-  },
+    async generateText(prompt) {
+        const apiKey = "<openai-key>";
+        return await Gen.get_marketing_desc(prompt, apiKey);
+    },
 
-  async generateImage(prompt) {
-    const openaiKey = "<openai-key>";
-    const stabilityKey = "<stability.ai-key>";
-    return await Gen.generate_image_from_desc(prompt, openaiKey, stabilityKey);
-  },
+    async generateImage(prompt) {
+        const openaiKey = "<openai-key>";
+        const stabilityKey = "<stability.ai-key>";
+        return await Gen.generate_image_from_desc(prompt, openaiKey, stabilityKey);
+    },
 
-  async generateAudio(text, base64 = true) {
-    const apiKey = "<google-cloud-key>";
-    const audioContent = await Gen.generate_speech_synthesis(text, apiKey);
-    return base64 ? audioContent : Buffer.from(audioContent, "base64");
-  },
+    async generateAudio(text, base64 = true) {
+        const apiKey = "<google-cloud-key>";
+        const audioContent = await Gen.generate_speech_synthesis(text, apiKey);
+        return base64 ? audioContent : Buffer.from(audioContent, "base64");
+    },
 };
 
 const app = express();
@@ -27,23 +29,34 @@ const path = require("path");
 app.use(express.static(path.join(__dirname)));
 
 app.post('/generate-content', async (req, res) => {
-  const { product, type } = req.body;
+    const {
+        product,
+        type
+    } = req.body;
 
-  if (type === 'text') {
-    const textPrompt = `Write a marketing copy for ${product}`;
-    const text = await intelliCode.generateText(textPrompt);
-    res.send({ text: text });
-  } else if (type === 'image') {
-    const imageData = await intelliCode.generateImage(product);
-    res.send({ imageData: imageData });
-  } else if (type === 'audio') {
-    const textPrompt = `Write a marketing copy for ${product}`;
-    const text = await intelliCode.generateText(textPrompt);
-    const audioData = await intelliCode.generateAudio(text);
-    res.send({ audioData: audioData });
-  } else {
-    res.status(400).send({ error: 'Invalid request type' });
-  }
+    if (type === 'text') {
+        const textPrompt = `Write a marketing copy for ${product}`;
+        const text = await intelliCode.generateText(textPrompt);
+        res.send({
+            text: text
+        });
+    } else if (type === 'image') {
+        const imageData = await intelliCode.generateImage(product);
+        res.send({
+            imageData: imageData
+        });
+    } else if (type === 'audio') {
+        const textPrompt = `Write a marketing copy for ${product}`;
+        const text = await intelliCode.generateText(textPrompt);
+        const audioData = await intelliCode.generateAudio(text);
+        res.send({
+            audioData: audioData
+        });
+    } else {
+        res.status(400).send({
+            error: 'Invalid request type'
+        });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
--- samples/content_generator_ui/front.js
+++ samples/content_generator_ui/front.js
@@ -1,74 +1,95 @@
 document.getElementById('content-form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const product = document.getElementById('product').value;
+    e.preventDefault();
+    const product = document.getElementById('product').value;
 
-  // Show loading spinner
-  document.getElementById('loading').classList.remove('d-none');
+    // Show loading spinner
+    document.getElementById('loading').classList.remove('d-none');
 
-  async function generateText() {
-    try {
-      const response = await fetch('/generate-content', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ product: product, type: 'text' }),
-      });
-      const { text } = await response.json();
-      document.getElementById('generated-text').innerText = text;
-      document.getElementById('text-title').classList.remove('d-none');
-    } catch (error) {
-      console.error('Error in generating text:', error);
+    async function generateText() {
+        try {
+            const response = await fetch('/generate-content', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    product: product,
+                    type: 'text'
+                }),
+            });
+            const {
+                text
+            } = await response.json();
+            document.getElementById('generated-text').innerText = text;
+            document.getElementById('text-title').classList.remove('d-none');
+        } catch (error) {
+            console.error('Error in generating text:', error);
+        }
     }
-  }
 
-  async function generateImage() {
-    try {
-      const response = await fetch('/generate-content', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ product: product, type: 'image' }),
-      });
-      const { imageData } = await response.json();
-      const imageDataUrl = `data:image/png;base64,${imageData}`;
-      const imageElement = document.getElementById('generated-image');
-      imageElement.src = imageDataUrl;
-      imageElement.classList.remove('d-none');
-      document.getElementById('image-title').classList.remove('d-none');
-    } catch (error) {
-      console.error('Error in generating image:', error);
+    async function generateImage() {
+        try {
+            const response = await fetch('/generate-content', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    product: product,
+                    type: 'image'
+                }),
+            });
+            const {
+                imageData
+            } = await response.json();
+            const imageDataUrl = `data:image/png;base64,${imageData}`;
+            const imageElement = document.getElementById('generated-image');
+            imageElement.src = imageDataUrl;
+            imageElement.classList.remove('d-none');
+            document.getElementById('image-title').classList.remove('d-none');
+        } catch (error) {
+            console.error('Error in generating image:', error);
+        }
     }
-  }
 
-  async function generateAudio() {
-    try {
-      const response = await fetch('/generate-content', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ product: product, type: 'audio' }),
-      });
-      const { audioData } = await response.json();
-      const audioDataUrl = `data:audio/mpeg;base64,${audioData}`;
-      const audioElement = document.getElementById('generated-audio');
-      audioElement.src = audioDataUrl;
-      audioElement.classList.remove('d-none');
-      document.getElementById('audio-title').classList.remove('d-none');
-    } catch (error) {
-      console.error('Error in generating audio:', error);
+    async function generateAudio() {
+        try {
+            const response = await fetch('/generate-content', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    product: product,
+                    type: 'audio'
+                }),
+            });
+            const {
+                audioData
+            } = await response.json();
+            const audioDataUrl = `data:audio/mpeg;base64,${audioData}`;
+            const audioElement = document.getElementById('generated-audio');
+            audioElement.src = audioDataUrl;
+            audioElement.classList.remove('d-none');
+            document.getElementById('audio-title').classList.remove('d-none');
+        } catch (error) {
+            console.error('Error in generating audio:', error);
+        }
     }
-  }
 
-  // Call the generate functions separately without waiting for each one to finish
-  Promise.all([
-    generateText().catch((error) => {
-      console.error('Error in generating text:', error);
-    }),
-    generateImage().catch((error) => {
-      console.error('Error in generating image:', error);
-    }),
-    generateAudio().catch((error) => {
-      console.error('Error in generating audio:', error);
-    }),
-  ]).finally(() => {
-    // Hide loading spinner
-    document.getElementById('loading').classList.add('d-none');
-  });
+    // Call the generate functions separately without waiting for each one to finish
+    Promise.all([
+        generateText().catch((error) => {
+            console.error('Error in generating text:', error);
+        }),
+        generateImage().catch((error) => {
+            console.error('Error in generating image:', error);
+        }),
+        generateAudio().catch((error) => {
+            console.error('Error in generating audio:', error);
+        }),
+    ]).finally(() => {
+        // Hide loading spinner
+        document.getElementById('loading').classList.add('d-none');
+    });
 });
