9b5954fda2f1f42960c6e6856d25f44bb9cff047
Bilal Amjad <bilalarain444@gmail.com>
2023-09-10 04:52:09 +0500

    remove line break

--- IntelliNode/test/integration/OpenAIWrapper.test.js
+++ IntelliNode/test/integration/OpenAIWrapper.test.js
@@ -32,7 +32,6 @@ async function testLanguageModel() {
 
 async function testFineTuneModel(){
     try {
-        
         const filePath = path.join(__dirname, 'Path_to_your_traning_file.jsonl'); // formated jsonl file supported by openai
         const upload = await openAI.uploadFile(filePath);
         console.log('Upload File Result:\n', upload, '\n');
