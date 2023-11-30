583eb0767cf9049f4b3b405101a1dedcb8ed3e93
Ahmad Barqawi <Barqawi.88@outlook.com>
2023-11-30 21:53:36 +0000

    update cohere embedding

--- IntelliNode/controller/RemoteEmbedModel.js
+++ IntelliNode/controller/RemoteEmbedModel.js
@@ -67,7 +67,16 @@ class RemoteEmbedModel {
       return results.data;
     } else if (this.keyType === SupportedEmbedModels.COHERE) {
       const results = await this.cohereWrapper.getEmbeddings(inputs);
-      return results.embeddings;
+      
+      let embeddings = results.embeddings;
+      embeddings = embeddings.map((embedding, index) => ({
+        object: "embedding",
+        index: index,
+        embedding: embedding
+      }));
+
+      return embeddings;
+
     } if (this.keyType === SupportedEmbedModels.REPLICATE) {
 
       const prediction = await this.replicateWrapper.predict('replicate', inputs);
