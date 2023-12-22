e4df89563d0d4f672e9f0c5356b778cc9382e8eb
Pushpak Sharma <sharmatryhard@gmail.com>
2023-12-22 16:04:49 +0530

    fixed test-cases

--- IntelliNode/test/integration/RemoteSpeechModel.test.js
+++ IntelliNode/test/integration/RemoteSpeechModel.test.js
@@ -40,28 +40,32 @@ async function testGenerateSpeech() {
 
 async function testOpenAiGenerateSpeech() {
   try {
-
-    const tempDir = '../temp';
-    if (!fs.existsSync(tempDir)) {
-      fs.mkdirSync(tempDir);
-    }
-
     const input = new Text2SpeechInput({
       model: 'tts-1',
       text: "The quick brown fox jumped over the lazy dog.",
       voice: "alloy",
-      stream: false
+      stream: true
     });
 
-    const audioContent = await openAiRemoteSpeechModel.generateSpeech(input);
+    const result = await openAiRemoteSpeechModel.generateSpeech(input);
 
-    assert(audioContent.length > 0, 'testGenerateSpeech response length should be greater than 0');
+    // Create a writable stream and pipe the response data to the stream
+    const filePath = 'downloaded_audio.mp3'; // Replace with the desired file name and extension
 
-    const decodedAudio = audioHelper.decode(audioContent);
-    const saved = audioHelper.saveAudio(decodedAudio, tempDir, 'temp.mp3');
-    assert(saved, 'Audio file should be saved successfully');
+    const writer = fs.createWriteStream(filePath);
+    result.pipe(writer);
 
-    console.log('Test passed: Audio generated and saved successfully');
+    // Handle the completion of writing the file
+    writer.on('finish', () => {
+      const fileExists = fs.existsSync(filePath);
+      assert(fileExists === true, 'file should be generated on finish')
+      console.log('Audio file downloaded successfully!');
+    });
+
+    // Handle any errors that may occur during the download process
+    writer.on('error', (err) => {
+      console.error('Error downloading the audio file:', err);
+    });
 
   } catch (error) {
     console.error('Test failed:', error);
