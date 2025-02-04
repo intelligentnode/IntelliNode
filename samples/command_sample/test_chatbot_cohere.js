e563c47e5238cb0b68b3f4ba2a2cb04d3f8bb121
cybercoder <barqawi.88@outlook.com>
2025-02-04 18:54:51 +0000

    update the sample

--- samples/command_sample/test_chatbot_cohere.js
+++ samples/command_sample/test_chatbot_cohere.js
@@ -9,7 +9,6 @@ async function callChatbot(apiKey, provider) {
   const system = 'You are a helpful assistant.';
   const input = new CohereInput(system);
   input.addUserMessage('what is the story of batman the dark night with less than 50 words');
-  input.numberOfOutputs = 1;
 
   const responses = await chatbot.chat(input);
 
@@ -23,8 +22,9 @@ async function callMultiMessageChatbot(apiKey, provider) {
 
   const system = 'You are a helpful assistant.';
   const input = new CohereInput(system);
-  input.addUserMessage('what is the story of batman the dark night with less than 50 words');
-  input.numberOfOutputs = 1;
+  input.addUserMessage("Explain the plot of the Inception movie  in one line");
+  input.addAssistantMessage("The plot of the movie Inception follows a skilled thief who enters people's dreams to steal their secrets and is tasked with implanting an idea into a target's mind to alter their future actions.");
+  input.addUserMessage("Explain the plot of the dark night movie in one line");
 
   const responses = await chatbot.chat(input);
 
@@ -38,7 +38,6 @@ async function callChatbotStream(apiKey, provider) {
   const system = 'You are a helpful assistant.';
   const input = new CohereInput(system);
   input.addUserMessage('what is the story of batman the dark night with less than 50 words');
-  input.numberOfOutputs = 1;
 
   let response = '';
   for await (const contentText of chatbot.stream(input)) {
@@ -51,9 +50,12 @@ async function callChatbotStream(apiKey, provider) {
 }
 
 (async () => {
-  // Test chatbot using OpenAI
+  // Test chatbot using Cohere
   console.log('test the chat function')
   await callChatbot(process.env.COHERE_API_KEY, 'cohere');
+
+  console.log('test the multi message')
+  await callMultiMessageChatbot(process.env.COHERE_API_KEY, 'cohere');
 
   console.log('test the stream function')
   await callChatbotStream(process.env.COHERE_API_KEY, 'cohere');
